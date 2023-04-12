863bf2c3275ba09c7fb4abd9330543e5a809a871
MaguDani <114920436+MaguDani@users.noreply.github.com>
2023-04-12 13:44:37 +0200

    Create Recorrer_tablero.py

---
+++ Recorrer_tablero.py
@@ -0,0 +1,12 @@
+from microbit import *
+from MicroRover import *
+
+rover = Micro_Rover()
+
+rover.move_forward(9)
+rover.girarIzquierda()
+rover.move_forward(9)
+rover.girarIzquierda()
+rover.move_forward(9)
+rover.girarIzquierda()
+rover.move_forward(9)
